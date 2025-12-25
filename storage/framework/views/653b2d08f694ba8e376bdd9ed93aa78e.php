68cc60ba380eb1bed2ee3603410428cc8e56d5ed
Devendra Hamal <devendra.hamal058@gmail.com>
2025-12-25 17:12:30 +0545

    Add storage/framework/cache/ to .gitignore and remove tracked cache files

---
+++ storage/framework/views/653b2d08f694ba8e376bdd9ed93aa78e.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\L E N O V O\Desktop\startups\dynamic_hr\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
